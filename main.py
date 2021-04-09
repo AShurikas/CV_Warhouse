abd30c74a192b004b83d2e8548f9ee86255ec59a
AShurikas <a.nechipor@gmail.com>
2021-04-09 11:51:59 +0300

    Added Screenshot module

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from PIL import Image
+import pyautogui
+import datetime
+
 infile1 = 'Got_order.png'
 infile2 = 'Empty_order.png'
 pix_coord = (114, 231)
@@ -7,7 +10,7 @@ pix_coord = (114, 231)
 def compare_pixels(image1, image2, pixel_coordinates=(114, 231)):
 
     """image1, 2 - input opened images
-        *pixel_coordinates - 4 arguments x1, y1, x2, y2 """
+        *pixel_coordinates - Tuple of  x, y comparing images """
     with Image.open(image1) as img1:
         with Image.open(image2) as img2:
             pix1 = img1.load()
@@ -15,4 +18,8 @@ def compare_pixels(image1, image2, pixel_coordinates=(114, 231)):
     return pix1[pixel_coordinates] == pix2[pixel_coordinates]
 
 
-print(compare_pixels(infile1, infile2, pixel_coordinates=pix_coord))
+def take_screenshot():
+    screenshot = pyautogui.screenshot()
+    timestamp = datetime.datetime.now().strftime('%H:%M:%S')
+    screenshot.save(timestamp + '.png')
+    return timestamp + '.png'
