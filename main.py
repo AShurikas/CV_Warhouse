5b2f89cf561ce6cf0430c770c9e538d8a0d36d24
AShurikas <a.nechipor@gmail.com>
2021-04-14 11:02:11 +0300

    Refactor. Not work None

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from PIL import Image
 import pyautogui
 import datetime
+import pytesseract
+import pyscreenshot as ImageGrab
+import numpy as np
+import cv2
 
 infile1 = 'Got_order.png'
 infile2 = 'Empty_order.png'
 pix_coord = (114, 231)
+print_coord = (372, 143)
+string_coord = (336, 206)
+red_point_coord = (511, 209)
+
+
+def click_left():
+    pyautogui.leftClick()
+
+
+def click_right():
+    pyautogui.rightClick()
+
+
+def move_mouse(coord):
+    pyautogui.moveTo(coord)
 
 
 def compare_pixels(image1, image2, pixel_coordinates):
@@ -19,9 +38,23 @@ def compare_pixels(image1, image2, pixel_coordinates):
 
 
 def take_screenshot():
-    """Save screenshot into current directory and
-    returns str object of it name consists current date %H:%M:%S"""
-    screenshot = pyautogui.screenshot()
-    timestamp = datetime.datetime.now().strftime('%H:%M:%S')
-    screenshot.save(timestamp + '.png')
-    return timestamp + '.png'
+    """Returns img object of screenshot
+    by coord area and save it in current directory by '%H%M' date format name"""
+    filename = str(datetime.datetime.now().strftime('%H%M')) + '.png'
+    screen = np.array(ImageGrab.grab(bbox=(185, 145, 293, 155)))
+    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    cv2.imwrite(filename, screen)
+    img = cv2.imread(filename)
+    return img
+
+
+def recognition_text():
+    text = pytesseract.image_to_string(take_screenshot(), lang='rus')
+    if text.strip():
+        return text.strip()
+
+
+if recognition_text() is not None:
+    print(recognition_text())
+else:
+    print('Hello')
