46b614faf3ff6f7a5ebe530b105f487386f49b32
AShurikas <a.nechipor@gmail.com>
2021-04-14 16:49:55 +0300

    Fixes in Telegram modules

--- main.py
+++ main.py
@@ -37,3 +37,29 @@ if recognition_text() is not None:
     pass
 else:
     pass
+
+
+#TODO TELEGRAM BOT!!!!!
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text(open('README.md', 'r', encoding='utf-8').read())
+
+
+def run_bot(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(response)
+
+
+def main():
+    """Start the bot."""
+    updater = Updater(API, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, run_bot))
+    updater.start_polling()
+    updater.idle()
