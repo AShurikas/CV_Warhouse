d28fa5bdeb89cc5bad565623de603d9992bc68a9
AShurikas <a.nechipor@gmail.com>
2021-04-14 11:31:20 +0300

    Deleted compary_pixels module

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from PIL import Image
 import pyautogui
 import datetime
 import pytesseract
 import pyscreenshot as ImageGrab
 import numpy as np
-import cv2
+
 
 infile1 = 'Got_order.png'
 infile2 = 'Empty_order.png'
@@ -24,17 +23,6 @@ def click_right():
 
 def move_mouse(coord):
     pyautogui.moveTo(coord)
-
-
-def compare_pixels(image1, image2, pixel_coordinates):
-    """image1, 2 - input images to compare
-        *pixel_coordinates - tuple of  x, y pixels coordinates to compare
-        returns False if images are different"""
-    with Image.open(image1) as img1:
-        with Image.open(image2) as img2:
-            pix1 = img1.load()
-            pix2 = img2.load()
-    return pix1[pixel_coordinates] == pix2[pixel_coordinates]
 
 
 def take_screenshot():
