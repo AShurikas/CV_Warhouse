174426d0a6670b9ab2cb6a3579a1ff187939956c
AShurikas <a.nechipor@gmail.com>
2021-04-14 16:20:06 +0300

    delete trash strings

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ import requests
 from Tele_API import API_Token as API
 
 
-infile1 = 'Got_order.png'
-infile2 = 'Empty_order.png'
-pix_coord = (114, 231)
-print_coord = (372, 143)
-string_coord = (336, 206)
-red_point_coord = (511, 209)
-
-
 def click_left():
     pyautogui.leftClick()
 
