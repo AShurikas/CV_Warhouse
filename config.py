abcef5ec14ba736fbb8d772f854de58ac5bf4f50
AShurikas <a.nechipor@gmail.com>
2021-04-19 15:35:21 +0300

    Fix config

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-API_Token = '1603267369:AAFiG9Y13qe2-69mijyYtDz_GvqXHQVssxw'
-channel_id = "-513639392"
-string_coord = (336, 206)
-print_coord = (372, 143)
-recognition_area = (185, 145, 293, 155)
-recognition_lang = 'rus'
-
-
 class Configs:
     def __init__(self, API_Token, channel_id, string_coord,
                  print_coord, recognition_area, recognition_lang):
@@ -15,4 +7,11 @@ class Configs:
         self.print_coord = print_coord
         self.recognition_area = recognition_area
         self.recognition_lang = recognition_lang
+
+
+    def set_API(self):
+
+
+
+
 
