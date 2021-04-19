4a639e42017a9ff4b9c74520099e10ea25cf3aae
AShurikas <a.nechipor@gmail.com>
2021-04-19 15:26:38 +0300

    Init config file

---
+++ config.py
@@ -0,0 +1,18 @@
+API_Token = '1603267369:AAFiG9Y13qe2-69mijyYtDz_GvqXHQVssxw'
+channel_id = "-513639392"
+string_coord = (336, 206)
+print_coord = (372, 143)
+recognition_area = (185, 145, 293, 155)
+recognition_lang = 'rus'
+
+
+class Configs:
+    def __init__(self, API_Token, channel_id, string_coord,
+                 print_coord, recognition_area, recognition_lang):
+        self.API_Token = API_Token
+        self.channel_id = channel_id
+        self.string_coord = string_coord
+        self.print_coord = print_coord
+        self.recognition_area = recognition_area
+        self.recognition_lang = recognition_lang
+
